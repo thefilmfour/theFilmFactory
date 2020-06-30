22dc030a9ae68a891c2924477b602dc1040343cd
Cheriele Mordido <eyelmordido@gmail.com>
2020-06-30 11:33:44 -0400

    Updates

---
+++ precache-manifest.f5790c3f82f1df124bfe7baac029b62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b1357560d3d39d2153f041778304d1",
+    "url": "/theFilmFactory/index.html"
+  },
+  {
+    "revision": "b93755c2dce7e5a5b0f4",
+    "url": "/theFilmFactory/static/css/main.7641f062.chunk.css"
+  },
+  {
+    "revision": "0327f85733c893939a32",
+    "url": "/theFilmFactory/static/js/2.8ce890f1.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/theFilmFactory/static/js/2.8ce890f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93755c2dce7e5a5b0f4",
+    "url": "/theFilmFactory/static/js/main.643e21d3.chunk.js"
+  },
+  {
+    "revision": "2be3a101d15026e01a4a",
+    "url": "/theFilmFactory/static/js/runtime-main.8cb3f050.js"
+  }
+]);
