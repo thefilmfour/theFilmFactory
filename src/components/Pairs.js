2b522f5db65ce4c1d8e69085ff714a25e358f700
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-07-01 11:06:58 -0400

    scroll-and-footer

--- src/components/Pairs.js
+++ src/components/Pairs.js
@@ -13,29 +13,7 @@ class Pairs extends Component {
       filmPairs: [],
     };
   }
-
-  // Event Handler for adding practice data to database - CAN BE DELETED EVENTUALLY
-  // handleClick = (event) => {
-  //   // event.preventDefault()
-  //   axios({
-  //     url: `https://api.themoviedb.org/3/movie/308/similar`,
-  //     params: {
-  //       api_key: "ac8a1621bd544ad33438bf381952729f",
-  //     },
-  //   }).then((response) => {
-  //     const filmPair = {
-  //       englishFilm: response.data.results[0],
-  //       foreignFilm: response.data.results[1],
-  //     };
-
-  //     const dbRef = firebase.database().ref();
-
-  //     dbRef.push(filmPair);
-  //   });
-  // };
-
-
-
+  
   componentDidMount() {
     // create a variable to store a reference to our database
     const dbRef = firebase.database().ref();
@@ -54,7 +32,7 @@ class Pairs extends Component {
         });
       }
       // 2. b) Reverse copy of State, so newest entries are display first
-      const reverseState = newState.reverse();
+      newState.reverse();
 
       // 3. Set State with changed copy
       this.setState({ filmPairs: newState });
@@ -65,8 +43,6 @@ class Pairs extends Component {
   render() {
     return (
       <section ref={this.props.PairsRef}>
-        {/* Button used to add practice data to database. CAN BE DELETED EVENTUALLY */}
-        {/* <button onClick={this.handleClick}>Click Me</button> */}
         <h2>Movies To Watch</h2>
         <ul>
           {this.state.filmPairs.map((filmPair) => {
